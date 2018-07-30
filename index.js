f646354a74e56b93af32e28b21a3448d7adf7242
Gaorieh Isaac <gaorieh@hotmail.com>
2018-07-30 21:58:18 +0200

    gg

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+let authentication = require("./authentication");
 
-const data = document.createElement('h2');
-data.innerText = "New text!"
-document.getElementById("text").appendChild(data);
+function getData(auth) {
+    var sheets = google.sheets('v4');
+    sheets.spreadsheets.values.get({
+        auth: auth,
+        spreadsheetId: 'yourSpreadSheetIDHere',
+        range: 'Sheet1!A2:C', //Change Sheet1 if your worksheet's name is something else
+    }, (err, response) => {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            return;
+        }
+        var rows = response.values;
+        if (rows.length === 0) {
+            console.log('No data found.');
+        } else {
+            for (var i = 0; i & lt; i++) {
+                var row = rows[i];
+                console.log(row.join(", "));
+            }
+        }
+    });
+}
+
+authentication.authenticate().then((auth) => {
+    getData(auth);
+});
